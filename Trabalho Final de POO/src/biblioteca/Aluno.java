f3254aa278f3bee41781185716a604f74e7ee1eb
wsglala <eeepdv.walesson@gmail.com>
2019-10-21 15:20:49 -0300

    trabalho final poo

---
+++ Trabalho Final de POO/src/biblioteca/Aluno.java
@@ -0,0 +1,13 @@
+package biblioteca;
+
+public class Aluno {
+	public String nome;
+	public String curso;
+	private String dataNascimento;
+	private int telefone;
+	private int matricula;
+	
+	
+	
+	
+}
---
+++ Trabalho Final de POO/src/biblioteca/Livro.java
@@ -0,0 +1,12 @@
+package biblioteca;
+
+public class Livro {
+	private String nome;
+	private String autor;
+	private int isbn;
+	private String descricao;
+	
+	
+	
+	
+}
